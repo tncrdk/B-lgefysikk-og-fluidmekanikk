2f60ad19e0d22e46f1df72a290aec2251d673715
tncrdk <85764203+tncrdk@users.noreply.github.com>
2023-03-08 20:44:13 +0100

    flyttet ut lab-mappen

--- Individuell-oving/Oving7/fourier_analysis.py
+++ Individuell-oving/Oving7/fourier_analysis.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 import scipy
 
 
-def plot_data(x_values: np.ndarray, ys = np.ndarray) -> None:
-    ax, fig = plt.figure()
+def plot_data(x_values: np.ndarray, ys=np.ndarray) -> None:
+    ax, fig = plt.subplots(1, 2)
+
+
 frequency_rate, amplitudes_arr = scipy.io.wavfile.read()
